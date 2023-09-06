3d7551a658e208378e8649286811b753d5f77905
KIDSSCC <18147667093@163.com>
2023-09-06 16:00:09 +0800

    best performance

--- src/SPH.cpp
+++ src/SPH.cpp
@@ -8,15 +8,15 @@ double PI = 3.141;
 
 //winSize为600，等效为6cm
 double winSize = 600;
-//光滑核半径，0.02米
-double H = 0.002;
-double MASS = 1.0;
-double RHO0 = 1000.0;
+//光滑核半径，0.002米
+double H = 0.0015;
+double MASS = 0.0001;
+double RHO0 = 1;
 double K = 20;
 double MU = 0.1;
 double G = -9.8;
 
-double Vel_atten = 0.7;
+double Vel_atten = 0.2;
 
 struct Particle {
     //粒子的位置，速度，加速度
@@ -29,7 +29,7 @@ struct Particle {
 };
 
 std::vector<Particle> particles;
-double timeStep = 0.001;
+double timeStep = 0.0001;
 double timeElapsed = 0.0;
 double maxX = 1.0, maxY = 1.0;
 double minX = 0.0, minY = 0.0;
@@ -70,6 +70,7 @@ double getValueOfPressureAccerlate(Particle&i, Particle&j)
     double res = 0;
     if (r <= H)
     {
+        //cout << "here,pressure is: " << (i.p + j.p) << endl;
         res = MASS * (i.p + j.p) / (2 * i.rho * j.rho) * W_Spiky(r);
     }
     return res;
@@ -80,6 +81,8 @@ vector<double> getViscosityAcerlate(Particle& i, Particle& j)
     vector<double> res{0,0};
     if (r <= H)
     {
+        //cout << "W_Viscosity is: " << W_Viscosity(r) << endl;
+        //cout << "multi rho is: " << i.rho * j.rho << endl;
         res[0] = MASS * MU * (j.vx - i.vx) / (i.rho * j.rho) * W_Viscosity(r);
         res[1] = MASS * MU * (j.vy - i.vy) / (i.rho * j.rho) * W_Viscosity(r);
     }
@@ -102,16 +105,16 @@ void calculateAcceleration(Particle& p) {
         }
 
         double pressureAccerlate = getValueOfPressureAccerlate(p, other);
-        cout << "distance is: " << distance(p, other) << endl;
-        cout << "pressureAccerlate is: " << pressureAccerlate << endl;
+        //cout << "distance is: " << distance(p, other) << endl;
+        //cout << "pressureAccerlate is: " << pressureAccerlate << endl;
         p.ax += pressureAccerlate * ((p.x - other.x) * winSize / 100);
         p.ay += pressureAccerlate * ((p.y - other.y) * winSize / 100);
-        cout << "pressure ax is: " << p.ax << " py is: " << p.ay << endl;
+        //cout << "pressure ax is: " << p.ax << " py is: " << p.ay << endl;
         auto viscosityAccerlate = getViscosityAcerlate(p, other);
         p.ax += viscosityAccerlate[0];
         p.ay += viscosityAccerlate[1];
-        cout << "viscosity is: " << viscosityAccerlate[0] << "  " << viscosityAccerlate[1] << endl;
-        cout << "final ax is: " << p.ax << " ay is: " << p.ay << endl;
+        //cout << "viscosity is: " << viscosityAccerlate[0] << "  " << viscosityAccerlate[1] << endl;
+        //cout << "final ax is: " << p.ax << " ay is: " << p.ay << endl;
     }
 }
 
@@ -119,9 +122,9 @@ void calculateAcceleration(Particle& p) {
 void updateParticles() {
     int i = 0;
     for (Particle& p : particles) {
-        cout << "num is: " << i++ << endl;
-        cout << "position is: " << p.x << " " << p.y << endl;
-        cout << "velocity is: " << p.vx << "  " << p.vy << endl;
+        //cout << "num is: " << i++ << endl;
+        //cout << "position is: " << p.x << " " << p.y << endl;
+        //cout << "velocity is: " << p.vx << "  " << p.vy << endl;
         p.vx += p.ax * timeStep;
         p.vy += p.ay * timeStep;
         p.x += p.vx * timeStep*10000/winSize;
@@ -142,10 +145,10 @@ void updateParticles() {
             p.vy = -Vel_atten * p.vy;
             p.y = minY;
         }
-        //cout << p.ax << "  " << p.ay << endl;
-        cout << "position is: " << p.x << " " << p.y << endl;
+        ////cout << p.ax << "  " << p.ay << endl;
+        //cout << "position is: " << p.x << " " << p.y << endl;
         calculateAcceleration(p);
-        cout << endl;
+        //cout << endl;
         //exit(0);
     }
 }
@@ -153,15 +156,15 @@ void updateParticles() {
 // Function to initialize particles
 void initializeParticles() {
 
-    particles.push_back({ 0.4, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 });
-    particles.push_back({ 0.4, 0.2, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 });
-    /*for (double x = 0.1; x < 0.9; x += 0.1) {
-        for (double y = 0.1; y < 0.9; y += 0.1) {
+    //particles.push_back({0.4, 0.2, 0, 0.0, 0.0, 0.0, 0.0, 0.0});
+    //particles.push_back({ 0.4, 0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0 });
+    for (double x = 0.1; x < 0.4; x += 0.1) {
+        for (double y = 0.1; y < 0.4; y += 0.1) {
             Particle p;
             p.x = x;
             p.y = y;
-            p.vx = 6;
-            p.vy = -4;
+            p.vx = 0.02;
+            p.vy = -0.01;
             p.ax = 0.0;
             p.ay = 0.0;
             p.rho = 0.0;
@@ -169,7 +172,7 @@ void initializeParticles() {
             particles.push_back(p);
         }
     }
-    */
+    
     
 }
 
@@ -177,11 +180,11 @@ void initializeParticles() {
 void calculateDensityAndPressure() {
     for (int i = 0; i < particles.size(); i++)
     {
-        particles[i].rho = 1001;
+        particles[i].rho = 0;
         for (int j = 0; j < particles.size(); j++)
         {
-            if (j == i)
-                continue;
+            /*if (j == i)
+                continue;*/
             double r = distance(particles[i], particles[j]);
             if (r < H)
             {
@@ -189,8 +192,8 @@ void calculateDensityAndPressure() {
             }
         }
         particles[i].p = pressure(particles[i]);
-        //cout << "pressure is: " << particles[i].p << endl;
-        //cout << "rho is: " << particles[i].rho << endl;
+        ////cout << "pressure is: " << particles[i].p << endl;
+        ////cout << "rho is: " << particles[i].rho << endl;
     }
 
 }
@@ -227,7 +230,7 @@ void update(int value) {
     calculateDensityAndPressure();
     timeElapsed += timeStep;
     glutPostRedisplay();
-    glutTimerFunc(100, update, 0);
+    glutTimerFunc(1, update, 0);
 }
 
 // Function to initialize OpenGL
